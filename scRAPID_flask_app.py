79ca51f1af8481d6eaed0d23e62e32be0b976b0a
jonathan-f <54023927+jonathan-f@users.noreply.github.com>
2023-07-03 11:00:50 +0200

    Updated flask app queries

--- scRAPID_flask_app.py
+++ scRAPID_flask_app.py
@@ -5,12 +5,6 @@ from io import StringIO
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    print("scrapid_db")
-    return "scRAPID test"
-
-# Please note that you'll need to replace the /download endpoint with the appropriate route in your Flask application.
 @app.route('/database', methods=['GET'])
 def download_csv():
     # Get query parameters from the request
@@ -20,31 +14,44 @@ def download_csv():
     # Perform the query on the database to retrieve the desired data subset
     data_subset = query_database(protein_query, rna_query)
 
-    # Generate the CSV file in-memory
-    csv_data = generate_csv(data_subset)
+    # Generate the CSV file
+    csv_file = generate_csv(data_subset)
 
-    # Create a response with the CSV data
-    response = Response(csv_data, mimetype='text/csv')
+    # Create a response with the CSV file
+    response = Response(csv_file.getvalue(), mimetype='text/csv')
     response.headers.set('Content-Disposition', 'attachment', filename='catRAPID_table_subset.csv')
 
     return response
 
 def query_database(protein_query, rna_query):
-    # Implement the database query based on the provided protein and RNA queries
-    # Modify the query based on the new query options for proteins and RNAs
-    query = """
-    SELECT * FROM catrapid
-    WHERE (Uniprot_ID = ? OR Entry_Name = ?)
-    AND (Ensembl_Gene_ID = ? OR Ensembl_Transcript_ID = ? OR gene_name = ?)
-    """
-
     # Create a connection to the SQLite database
     conn = sqlite3.connect('catrapid.db')
     cursor = conn.cursor()
 
     try:
-        # Execute the query with the provided parameters
-        cursor.execute(query, (protein_query, protein_query, rna_query, rna_query, rna_query))
+        if protein_query and rna_query:
+            query = """
+            SELECT * FROM catrapid
+            WHERE (Uniprot_ID = ? OR Entry_Name = ?)
+            AND (Ensembl_Gene_ID = ? OR Ensembl_Transcript_ID = ? OR gene_name = ?)
+            """
+            cursor.execute(query, (protein_query, protein_query, rna_query, rna_query, rna_query))
+
+        elif protein_query:
+            # Single protein
+            query = '''
+            SELECT * FROM catrapid
+            WHERE Uniprot_ID = ? OR Entry_Name = ?
+            '''
+            cursor.execute(query, (protein_query, protein_query))
+
+        elif rna_query:
+            # Single RNA
+            query = '''
+            SELECT * FROM catrapid
+            WHERE Ensembl_Gene_ID = ? OR Ensembl_Transcript_ID = ? OR gene_name = ?
+            '''
+            cursor.execute(query, (rna_query, rna_query, rna_query))
 
         # Fetch all rows from the result set
         data_subset = cursor.fetchall()
@@ -61,14 +68,13 @@ def query_database(protein_query, rna_query):
         conn.close()
 
 def generate_csv(data_subset):
-    csv_data = StringIO()
-    writer = csv.writer(csv_data)
+    csv_file = StringIO()
+    writer = csv.writer(csv_file)
 
     # Write the CSV header
     writer.writerow(['Uniprot_ID', 'Entry_Name', 'Protein_Frag', 'Ensembl_Gene_ID', 'Ensembl_Gene_ID_version', 'Ensembl_Transcript_ID', 'Ensembl_Transcript_ID_version', 'gene_name', 'RNA_Frag', 'score'])
 
     # Write the data rows
-    for row in data_subset:
-        writer.writerow(row)
+    writer.writerows(data_subset)
 
-    return csv_data.getvalue()
+    return csv_file
